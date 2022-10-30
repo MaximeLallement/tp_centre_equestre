b9f7242d1ea839a31f45fa3827cb2b79ed6e0761
NewAqhbd <95507132+NewAqhbd@users.noreply.github.com>
2022-10-30 13:42:29 +0100

    Ajout du CRUD Representant

--- vue/rep/rep_index.php
+++ vue/rep/rep_index.php
@@ -1,30 +1,59 @@
 <?php
 
-require('../../inc/bdd.inc.php');
-require "../../model/Representant.php";
-$data = get_all_rep();
+require_once(".././inc/bdd.inc.php");
+require_once(".././model/Representant.php");
+$pagename = "Représentant";
+require $headerpath; //Importe le header
 
 ?>
-<table>
-<?php foreach($data as $row){ ?>
-    <tr>
-        <td style='width: 5%'>
-        <input type='checkbox' class='checkbox' data-index="" checked='false'></td>
-        <td><center><?php echo $row["id_personne"] ?></center></td>
-        <td><center><?php echo $row["nom_personne"] ?></center></td>
-        <td style='display:flex; justify-content: space-evenly;'>
-        <form action="Modifier_Representant.php" method="post">
-            <button type='button' onclick="location.href = 'Modifier_Representant.php'">
-                Modifier
-            </button>
-            <button type="button" onclick="location.href = 'Supprimer_Representant.php'">
-                Supprimer
-            </button>
-        </form>
-            <form action="Modifier_Representant.php" method="post">
-                <input type="hidden" name="id_personne" value="<?php echo $row["id_personne"] ?>">
-            </form>
-        </td>
-    </tr>
-    <?php   }   ?>
+<? //Affiche la liste des représentants ?>
+<table id='rep_list'>
+    <thead>
+        <tr>
+            <th>Identifiant</th>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Licence</th>
+            <th>Modifier</th>
+            <th>Supprimer</th>
+        </tr>
+    </thead> 
+    <tbody>
+        <?php foreach(get_all_rep() as $rep){ ?> <!-- Génère un tableau des représentants avec leurs infos personnelles -->
+            <tr>
+                <td><?php echo $rep["id_personne"] ?></td>
+                <td><?php echo utf8_encode($rep["nom_personne"]) ?></td>
+                <td><?php echo utf8_encode($rep["prenom_personne"]) ?></td>
+                <td><?php echo $rep["num_licence"] ?></td>
+
+                <td>
+                    <form action=".././controller/RepresentantController.php" method="post">
+                        <input type="hidden" name="rep_id" value="<?= $rep["id_personne"]; ?>"> <!-- Permet de récupérer l'id du représentant sélectionné -->
+                        <button type='submit' name="modify"> <!-- Option modifier -->
+                            Modifier
+                        </button>
+                    </form>
+                </td>
+
+                <td>
+                    <form action=".././controller/RepresentantController.php" method="post">
+                        <input type="hidden" name="rep_id" value="<?= $rep["id_personne"]; ?>"> <!-- Permet de récupérer l'id du représentant sélectionné -->
+                        <button type="submit" name="delete"> <!-- Option supprimer -->
+                            Supprimer
+                        </button>
+                    </form>
+                </td>
+            </tr>
+        <?php   }   ?>
+    </tbody>
 </table>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.12.1/datatables.min.js"></script>
+
+    <script>
+        $(document).ready(function () {
+            $('#rep_list').DataTable(); //Applique la fontion DataTable() à l'élément dont l'id = 'rep_list'
+        });   
+    </script>
